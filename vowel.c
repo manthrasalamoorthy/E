bbc5e4962a9fd5d3a10564af9836778aa46ae671
manthrasalamoorthy <30748252+manthrasalamoorthy@users.noreply.github.com>
2017-08-05 16:13:56 +0530

    vowel.c
    vowel or not

---
+++ vowel.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[]={'a','e','i','o','u'};
+char n;
+printf("enter the 'b' value:");
+scanf("%c",&n);
+if(n=='a'||n=='e'||n=='i'||n=='o'||n=='o')
+{
+printf("it is vowel");
+}
+else
+{
+printf("it is constant");
+}
+getch();
+}
